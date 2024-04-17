d8d57445aea339d11e8a3590835b9c393c6dc217
ericksyahputra <syahputraae34@gmail.com>
2024-04-17 09:14:55 +0700

    second commit

--- dashboard.php
+++ dashboard.php
@@ -25,7 +25,7 @@
                     <div class="dropdown">
                         <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="bi bi-person-circle"></i>
-                            Erick Syahputra
+                            Administrator
                         </button>
                         <ul class="dropdown-menu">
                             <li>
